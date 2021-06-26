d1639ed3883c01aedc88133e9015e0a1d59c075f
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-26 17:04:22 +0600

    Update UserController.php

--- example-app-v0.1/app/Http/Controllers/UserController.php
+++ example-app-v0.1/app/Http/Controllers/UserController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function userData(Request $request){
+        $request->validate([
+            'username'=>'required | max:10',
+            'password'=>'required | min:4'
+        ]);
         return $request;
     }
 }
