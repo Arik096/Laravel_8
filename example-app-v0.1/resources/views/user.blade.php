d0935fecf9ef38f6fde5c3f8882760815589b574
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-26 17:04:25 +0600

    Update user.blade.php

--- example-app-v0.1/resources/views/user.blade.php
+++ example-app-v0.1/resources/views/user.blade.php
@@ -1,11 +1,23 @@
 <h1>User View</h1>
 
-
-<form action="user" method="POST">
+@if ($errors->any())
+@foreach ($errors->all() as $err)
+<li>{{$err}}</li>
+@endforeach
+@endif
+<form action="user" method="POST" autocomplete="on">
     @csrf
     <input type="text" name="username" id="username" placeholder="username">
     <br>
+    <span style="color: red">@error('username')
+        {{$message}}
+    @enderror</span>
+    <br>
     <input type="password" name="password", id="password" placeholder="password">
+    <br>
+    <span style="color: red">@error('password')
+        {{$message}}
+    @enderror</span>
     <br>
     <button type="submit"> Submit</button>
 </form>
