f31e8731592688c883b0ab81d78320e5ffc80742
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-27 20:39:46 +0600

    Update VisitorRequestController.php

--- example-app-v0.1/app/Http/Controllers/VisitorRequestController.php
+++ example-app-v0.1/app/Http/Controllers/VisitorRequestController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class VisitorRequestController extends Controller
 {
     public function visitorData(Request $request){
+        $request->validate([
+            'name'=> 'min:5 | required'
+        ]);
         return $request;
     }
 }
