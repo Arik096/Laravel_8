26f8a48733be856f73fa83823b91cb2bf7288c47
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-27 20:39:49 +0600

    Update visitor.blade.php

--- example-app-v0.1/resources/views/visitor.blade.php
+++ example-app-v0.1/resources/views/visitor.blade.php
@@ -1,12 +1,16 @@
 <h1>Visitor View</h1>
-
+@if ($errors->any())
+@foreach ($errors->all() as $error)
+<li>{{$error}}</li>
+@endforeach
+@endif
 <form action="visitorReq" method="POST">
     @csrf
-    <input type="text" name="name" placeholder="name">
+    <input type="text" name="name" placeholder="name" value="{{old('name')}}">
     <br>
-    <input type="text" name="mobile" placeholder="mobile">
+    <input type="text" name="mobile" placeholder="mobile" value="{{old('mobile')}}">
     <br>
-    <input type="text" name="email" placeholder="email">
+    <input type="text" name="email" placeholder="email" value="{{old('email')}}">
     <br>
     <button type="submit">Request</button>
 </form>
