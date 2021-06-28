610a47cc63f83e9e767a9667874af8e49cea30cb
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-28 22:42:31 +0600

    Update UserController.php

--- example-app-v0.2/app/Http/Controllers/UserController.php
+++ example-app-v0.2/app/Http/Controllers/UserController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index(){
-         return DB::select("select * from users");
+    // public function index(){
+    //      return DB::select("select * from users");
+    // }
+
+
+    public function getData(){
+        return User::all();
     }
 }
