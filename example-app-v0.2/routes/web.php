b6ffea136cd7e369592f6f23f5a772040f343877
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-29 20:59:16 +0600

    Update web.php
    flash session

--- example-app-v0.2/routes/web.php
+++ example-app-v0.2/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\loginController;
+use App\Http\Controllers\addMemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,25 +25,29 @@ Route::get('/', function () {
 // Route::get('/user',[UserController::class, 'getData']);
 // Route::get('/user',[UserController::class, 'apiData']);
 
-Route::view('/user', 'user');
+// Route::view('/user', 'user');
 // Route::post('/userloged', [UserController::class, 'testRequest']);
-Route::delete('/userloged', [UserController::class, 'testRequest']);
+// Route::delete('/userloged', [UserController::class, 'testRequest']);
 // Route::put('/userloged', [UserController::class, 'testRequest']);
 // Route::get('/userloged', [UserController::class, 'testRequest']);
 
 
 // Route::view('/login','login');
-Route::view('/profile','profile');
-Route::post('/logged', [loginController::class, 'index']);
-Route::get('/login', function () {
-    if (session()->has('username')) {
-        return redirect('/profile');
-    }
-    return view('login');
-});
-Route::get('/logout', function () {
-    if(session()->has('username')){
-        session()->pull('username');
-    }
-    return redirect('/login');
-});
+// Route::view('/profile','profile');
+// Route::post('/logged', [loginController::class, 'index']);
+// Route::get('/login', function () {
+//     if (session()->has('username')) {
+//         return redirect('/profile');
+//     }
+//     return view('login');
+// });
+// Route::get('/logout', function () {
+//     if(session()->has('username')){
+//         session()->pull('username');
+//     }
+//     return redirect('/login');
+// });
+
+
+Route::view('/member', 'member');
+Route::post('/added', [addMemberController::class,'addMember']);
