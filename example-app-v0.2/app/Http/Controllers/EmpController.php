67efb01fa854d83d44fc4ac5468512138650195d
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-06-30 20:54:11 +0600

    Update EmpController.php
    pagination

--- example-app-v0.2/app/Http/Controllers/EmpController.php
+++ example-app-v0.2/app/Http/Controllers/EmpController.php
@@ -6,12 +6,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Emp;
 
+
 class EmpController extends Controller
 {
     public function show(){
         // return view('tableList');
-        $data =  Emp::all();
-
+        $data =  Emp::paginate(7);
         return view('tableList', ['emps'=>$data]);
     }
 }
