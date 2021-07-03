efac26811531fcc19e9f59fc7900b9c1bf650512
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-03 11:28:59 +0600

    Update web.php
    mutator

--- example-app-v0.2/routes/web.php
+++ example-app-v0.2/routes/web.php
@@ -83,3 +83,4 @@ Route::get('/db',[EmpController::class, 'dbOperations']);
 
 Route::get('/staff',[StaffController::class, 'index']);
 Route::get('/acc',[StaffController::class, 'accessors']);
+Route::get('/mut',[StaffController::class, 'mutators']);
