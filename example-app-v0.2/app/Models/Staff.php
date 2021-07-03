f1f5fff2d3797be449b9ba76890904b7fb66c7c7
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-03 11:28:51 +0600

    Update Staff.php
    mutator

--- example-app-v0.2/app/Models/Staff.php
+++ example-app-v0.2/app/Models/Staff.php
@@ -12,11 +12,24 @@ class Staff extends Model
 
 
 
-    public function getNameAttribute($value){
-        return ucfirst($value);
-    }
+    // public function getNameAttribute($value){
+    //     return ucfirst($value);
+    // }
 
-    public function getEmailAttribute($value){
-        return $value.' ,bd';
+    // public function getEmailAttribute($value){
+    //     return $value.' ,bd';
+    // }
+
+
+    public function setNameAttribute($value){
+        if(substr($value, 0 ,3) == 'Mr.' || substr($value, 0, 3) == 'mr.'){
+            $this->attributes['name'] = $value;
+        }
+        else{
+        $this->attributes['name'] = 'Mr. '. $value;
+        }
+    }
+    public function setEmailAttribute($value){
+        $this->attributes['email'] = $value. ', bd';
     }
 }
