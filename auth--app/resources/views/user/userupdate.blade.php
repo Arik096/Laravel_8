f0cc500bac1a50d68bf571cdc9b7904d67d327de
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-06 20:31:35 +0600

    Create userupdate.blade.php
    user update

---
+++ auth--app/resources/views/user/userupdate.blade.php
@@ -0,0 +1,25 @@
+<h1>Edit {{$user[0]->name}} Data</h1>
+
+@if ($errors->any())
+@foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+@endforeach
+@endif
+<form action="/userlist" method="POST">
+    @csrf
+    <input type="hidden" name="id" id="id" value="{{$user[0]->id}}">
+    <label for="name">Name</label>
+    <br>
+    <input type="text" name="name" id="name" value="{{$user[0]->name}}">
+    <br>
+    <label for="email">Email</label>
+    <br>
+    <input type="email" name="email" id="{{$user[0]->email}}">
+    <br>
+    <label for="password">Password</label>
+    <br>
+    <input type="password" name="password" id="password" value="">
+    <br>
+    <br>
+    <button type="submit">Update</button>
+</form>
