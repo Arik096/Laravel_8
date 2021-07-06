dd350eaa96fd2145901141e71e5a0a56d06999e7
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-06 20:31:25 +0600

    Update UserController.php
    user update

--- auth--app/app/Http/Controllers/UserController.php
+++ auth--app/app/Http/Controllers/UserController.php
@@ -9,7 +9,29 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     function userList(){
-        $userData = User::get();
+        $userData = DB::table('users')->get();
         return view('user.userlist',['users'=>$userData]);
+    }
+
+    function userUpdate($id){
+        $singleUserData = DB::table('users')->where('id',1)->get();
+        //$singleUserData = $Data->toJson();
+        return view('user.userupdate',['user'=> $singleUserData]);
+    }
+
+    function saveUserData(Request $request){
+        $dataOk = $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password' => 'required'
+        ]);
+
+        DB::table('users')->where('id',$request->id)->update([
+            'name'=> $request->name,
+            'email'=>$request->email,
+            'password'=>$request->password
+        ]);
+
+        return redirect('/userlist')->with('update','Data Updated');
     }
 }
