82c1283f44615a8ac23050c0b71f5082767faca0
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-16 16:36:24 +0600

    Update StudentList.php
    std delete

--- livewire--app/app/Http/Livewire/StudentList.php
+++ livewire--app/app/Http/Livewire/StudentList.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\Session\Session;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 use Livewire\WithPagination;
 
 class StudentList extends Component
 {
+
+    public function delete($id){
+        DB::table('students')->where('id', $id)->delete();
+        Session()->flash('sms', 'Student Deleted');
+        return Redirect()->to(route('stdlist'));
+    }
+
+
     public function render()
     {
         $students = DB::table('students')->get();
