0eaca4e06f4be1f118ab75af2f9a7d799afacd2c
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-17 16:52:18 +0600

    Update web.php
    file upload

--- livewire--app/routes/web.php
+++ livewire--app/routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Livewire\Students;
 use App\Http\Livewire\StudentList;
 use App\Http\Livewire\StudentCreate;
 use App\Http\Livewire\StudentUpdate;
+use App\Http\Livewire\Uplaods;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,3 +45,6 @@ Route::prefix('crud')->group(function () {
     Route::get('/create',StudentCreate::class)->name('stdcreate');
     Route::get('/upadte/{id}',StudentUpdate::class)->name('stdupdate');
 });
+
+
+Route::get('/up',Uplaods::class)->name('up');
