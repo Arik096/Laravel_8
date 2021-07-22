7793430c0211c1ad2fbbf4afc7be9051f9a80f1d
Arik Md. Isthiaque <arikbncc@gmail.com>
2021-07-22 15:28:37 +0600

    Update StudentAPI.php
    post api

--- api--app/app/Http/Controllers/StudentAPI.php
+++ api--app/app/Http/Controllers/StudentAPI.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Student;
 
 class StudentAPI extends Controller
 {
@@ -14,5 +16,23 @@ class StudentAPI extends Controller
     public function getSTDbyID($id = null)
     {
         return DB::table('students')->where('id', $id)->get();
+    }
+
+    public function setSTD(Request $request){
+        $std = new Student;
+        $std->name =$request->name;
+        $std->dept =$request->dept;
+        $std->email =$request->email;
+        $std->phone =$request->phone;
+
+
+        $result = $std->save();
+
+        if($result){
+            return "Data Added";
+        }
+        else{
+            return "Error";
+        }
     }
 }
